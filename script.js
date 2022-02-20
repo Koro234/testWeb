0c7ae94e44ce87fb93ee5f6801b1f0b06e131952
Koro234 <tra.ne555222@mail.ru>
2022-02-20 20:18:25 +0400

    twoWeeks

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// const users = [{"id":1,"username":"user1","surname":"Мозгов","firstName":"Антон","secondName":""},{"id":2,"username":"user2","surname":"Тонких","firstName":"Антон","secondName":""},{"id":3,"username":"user3","surname":"Тонких","firstName":"Григорий","secondName":""},{"id":4,"username":"user4","surname":"Васильев","firstName":"Влад","secondName":""},{"id":5,"username":"user5","surname":"Быстров","firstName":"Григорий","secondName":""},{"id":6,"username":"user6","surname":"Смагин","firstName":"Дмитрий","secondName":""},{"id":7,"username":"user7","surname":"Мозгов","firstName":"Александр","secondName":""},{"id":8,"username":"user8","surname":"Кузнецов","firstName":"Альберт","secondName":""},{"id":9,"username":"user9","surname":"Петров","firstName":"Борис","secondName":""},{"id":10,"username":"user10","surname":"Васильев","firstName":"Дмитрий","secondName":""},{"id":11,"username":"user11","surname":"Мозгов","firstName":"Дмитрий","secondName":""},{"id":12,"username":"user12","surname":"Быстров","firstName":"Иван","secondName":""},{"id":13,"username":"user13","surname":"Петренко","firstName":"Борис","secondName":""},{"id":14,"username":"user14","surname":"Некрасов","firstName":"Денис","secondName":""},{"id":15,"username":"user15","surname":"Петров","firstName":"Александр","secondName":""},{"id":16,"username":"user16","surname":"Добрый","firstName":"Борис","secondName":""},{"id":17,"username":"user17","surname":"Быстров","firstName":"Борис","secondName":""},{"id":18,"username":"user18","surname":"Васильев","firstName":"Александр","secondName":""},{"id":19,"username":"user19","surname":"Кузнецов","firstName":"Алексей","secondName":""},{"id":20,"username":"user20","surname":"Мозгов","firstName":"Альберт","secondName":""},{"id":21,"username":"user21","surname":"Кожин","firstName":"Сергей","secondName":""},{"id":22,"username":"user22","surname":"Васильев","firstName":"Альберт","secondName":""},{"id":23,"username":"user23","surname":"Мозгов","firstName":"Алексей","secondName":""},{"id":24,"username":"user24","surname":"Быстров","firstName":"Олег","secondName":""},{"id":25,"username":"user25","surname":"Петров","firstName":"Денис","secondName":""}];
-// const tasks = [{"id":"65b9242b-1976-458a-801e-67a614c59d76","subject":"Анализ","description":"","creationAuthor":1,"executor":1,"creationDate":"2022-01-22","planStartDate":"2022-01-22","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"7ebc65b3-98d9-422f-ba7f-9615bd214068","subject":"Планирование","description":"","creationAuthor":1,"executor":1,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"44d22c4d-03cf-4fa9-9e4b-39353b68325a","subject":"Проектирование","description":"","creationAuthor":1,"executor":2,"creationDate":"2022-01-22","planStartDate":"2022-01-25","planEndDate":"2022-01-26","endDate":"2022-01-22","status":1,"order":1},{"id":"a0d7abc4-9b22-43f8-bf5a-1881ec3afa86","subject":"Разработка","description":"","creationAuthor":1,"executor":3,"creationDate":"2022-01-22","planStartDate":"2022-01-25","planEndDate":"2022-01-28","endDate":"2022-01-22","status":1,"order":1},{"id":"bbc69d23-3feb-450a-a6ce-7293c45d012f","subject":"Тестирование","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-27","planEndDate":"2022-01-28","endDate":"2022-01-22","status":1,"order":1},{"id":"3f63f1fe-c374-40d3-8dbe-ca0a71889aea","subject":"Заместитель","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"68bde071-8417-40de-818d-f821c3d30f20","subject":"Наблюдатель","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"280ad70e-0b54-4d8c-964e-9bb3b2573c28","subject":"Декоратор","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"8404aecd-94d1-48ff-a354-23cd39c57837","subject":"Фасад","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},
+// let users = [{"id":1,"username":"user1","surname":"Мозгов","firstName":"Антон","secondName":""},{"id":2,"username":"user2","surname":"Тонких","firstName":"Антон","secondName":""},{"id":3,"username":"user3","surname":"Тонких","firstName":"Григорий","secondName":""},{"id":4,"username":"user4","surname":"Васильев","firstName":"Влад","secondName":""},{"id":5,"username":"user5","surname":"Быстров","firstName":"Григорий","secondName":""},{"id":6,"username":"user6","surname":"Смагин","firstName":"Дмитрий","secondName":""},{"id":7,"username":"user7","surname":"Мозгов","firstName":"Александр","secondName":""},{"id":8,"username":"user8","surname":"Кузнецов","firstName":"Альберт","secondName":""},{"id":9,"username":"user9","surname":"Петров","firstName":"Борис","secondName":""},{"id":10,"username":"user10","surname":"Васильев","firstName":"Дмитрий","secondName":""},{"id":11,"username":"user11","surname":"Мозгов","firstName":"Дмитрий","secondName":""},{"id":12,"username":"user12","surname":"Быстров","firstName":"Иван","secondName":""},{"id":13,"username":"user13","surname":"Петренко","firstName":"Борис","secondName":""},{"id":14,"username":"user14","surname":"Некрасов","firstName":"Денис","secondName":""},{"id":15,"username":"user15","surname":"Петров","firstName":"Александр","secondName":""},{"id":16,"username":"user16","surname":"Добрый","firstName":"Борис","secondName":""},{"id":17,"username":"user17","surname":"Быстров","firstName":"Борис","secondName":""},{"id":18,"username":"user18","surname":"Васильев","firstName":"Александр","secondName":""},{"id":19,"username":"user19","surname":"Кузнецов","firstName":"Алексей","secondName":""},{"id":20,"username":"user20","surname":"Мозгов","firstName":"Альберт","secondName":""},{"id":21,"username":"user21","surname":"Кожин","firstName":"Сергей","secondName":""},{"id":22,"username":"user22","surname":"Васильев","firstName":"Альберт","secondName":""},{"id":23,"username":"user23","surname":"Мозгов","firstName":"Алексей","secondName":""},{"id":24,"username":"user24","surname":"Быстров","firstName":"Олег","secondName":""},{"id":25,"username":"user25","surname":"Петров","firstName":"Денис","secondName":""}];
+// let tasks = [{"id":"65b9242b-1976-458a-801e-67a614c59d76","subject":"Анализ","description":"","creationAuthor":1,"executor":1,"creationDate":"2022-01-22","planStartDate":"2022-01-22","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"7ebc65b3-98d9-422f-ba7f-9615bd214068","subject":"Планирование","description":"","creationAuthor":1,"executor":1,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"44d22c4d-03cf-4fa9-9e4b-39353b68325a","subject":"Проектирование","description":"","creationAuthor":1,"executor":2,"creationDate":"2022-01-22","planStartDate":"2022-01-25","planEndDate":"2022-01-26","endDate":"2022-01-22","status":1,"order":1},{"id":"a0d7abc4-9b22-43f8-bf5a-1881ec3afa86","subject":"Разработка","description":"","creationAuthor":1,"executor":3,"creationDate":"2022-01-22","planStartDate":"2022-01-25","planEndDate":"2022-01-28","endDate":"2022-01-22","status":1,"order":1},{"id":"bbc69d23-3feb-450a-a6ce-7293c45d012f","subject":"Тестирование","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-27","planEndDate":"2022-01-28","endDate":"2022-01-22","status":1,"order":1},{"id":"3f63f1fe-c374-40d3-8dbe-ca0a71889aea","subject":"Заместитель","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"68bde071-8417-40de-818d-f821c3d30f20","subject":"Наблюдатель","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"280ad70e-0b54-4d8c-964e-9bb3b2573c28","subject":"Декоратор","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"8404aecd-94d1-48ff-a354-23cd39c57837","subject":"Фасад","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},
 // {"id":"33b5471e-02b8-49f5-b4e4-909c208b0c84","subject":"Адаптер","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"f8fad7f5-47f2-42bf-86dd-0a1c6201e612","subject":"Строитель","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"8da4b76f-97f9-4e6f-a186-0555a44b6daa","subject":"Мост","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"be6212ce-f33d-4be7-b900-00b8f2d22948","subject":"Команда","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"152908c3-4580-4da9-8ad6-a3e027bb079a","subject":"Итератор","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"296221a1-22ad-43bc-af38-90519ae4b167","subject":"Посредник","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"56cb0a46-b78c-461b-b6e8-6db8c0a97d85","subject":"Прототип","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"3cc17886-1e0c-48f6-b69c-6d8e617739b1","subject":"Компоновщик","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"755c0445-b083-4c96-bbed-3e5647ba33db","subject":"Снимок","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},
 // {"id":"89c606d3-7d7f-4e20-9051-6c911943ceae","subject":"Состояние","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"c414118b-a08e-4419-8a24-5fc0680ba1f2","subject":"Стратегия","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"b4156a9c-2d4f-4f58-9e2c-d9a86b412d36","subject":"Одиночка","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"c5c47775-f161-43c7-92f9-bdde07c083ba","subject":"Легковес","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"e2ac1dd2-62a6-4754-bf74-1c3a21eae2a4","subject":"Посетитель","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"e0989c77-70e1-4015-beef-7b21cba1538d","subject":"Фабричный метод","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"28f49ccb-7280-4058-867e-e987409d937e","subject":"Абстрактная фабрика","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1},{"id":"ce1c551a-17f8-4ca5-9cea-6c02a8fe620e","subject":"Цепочка обязанностей","description":"","creationAuthor":1,"executor":null,"creationDate":"2022-01-22","planStartDate":"2022-01-24","planEndDate":"2022-01-25","endDate":"2022-01-22","status":1,"order":1}];
 
@@ -170,7 +170,22 @@ function FillPopup (Task) { // заполнение попапа данными 
 function myDateToString(date) {
     return (`${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`);
 }
-//Создание списка пропапа
+
+function getDayOnDesc(date) {
+    var tempDay;
+    for (var i = 0; i < dateArray.length; i++)
+    {
+        if (dateArray[i].getFullYear() == date.getFullYear()
+        && dateArray[i].getMonth() == date.getMonth()
+        && dateArray[i].getDate() == date.getDate())
+        {
+            tempDay = i;
+        }
+    }
+    return tempDay + 1;
+}
+
+//Создание списка попапа
 function CreateListExecutor(id) {
     executor.innerHTML = '';
     var option = document.createElement('option');
@@ -235,7 +250,7 @@ function CreateUserContainer(User) {
     userName.ondrop = DropOnName;
     userName.innerText = `${User.firstName} ${User.secondName} ${User.surname}`;
     userContainer.appendChild(userName);
-    for (var i = 0; i < 7; i++)
+    for (var i = 0; i < 14; i++)
     {
         let dayElement = document.createElement('div');
         dayElement.classList.add('day');
@@ -272,13 +287,11 @@ function CreateTaskOnDesk(User, Task) {
     Task.user = User;
     var startDate = Task.planStartDate;
     var endDate = Task.planEndDate;
-    var startDay = startDate.getDay();
-    startDay = startDay === 0 ? 7 : (startDay);
-    var endDay = endDate.getDay();
-    endDay = endDay === 0 ? 7 : (endDay);
+    var startDay = getDayOnDesc(startDate);
+    var endDay = getDayOnDesc(endDate);
     if (startDate <= endDate) 
     {
-        if ((startDate >= leftDate && startDate <= rightDate) && (endDate >= leftDate && endDate <= rightDate))
+        if (startDate >= leftDate && startDate <= rightDate && endDate >= leftDate && endDate <= rightDate)
         {
             var span = endDay - startDay + 1;
             var event = ConstructorTaskElement(Task, startDay, span);
@@ -286,9 +299,9 @@ function CreateTaskOnDesk(User, Task) {
             Task.htmlElement = event;
             User.htmlElement.lastChild.appendChild(event);
         }
-        else if (startDate >= leftDate && startDate <= rightDate && endDate >= rightDate)
+        else if (startDate >= leftDate && startDate <= rightDate && endDate > rightDate)
         {
-            var span = 8 - startDay;
+            var span = 15 - startDay;
             var event = ConstructorTaskElement(Task, startDay, span);
             event.classList.add('event-start');
             Task.htmlElement = event;
@@ -304,7 +317,7 @@ function CreateTaskOnDesk(User, Task) {
         }
         else if (startDate <= leftDate && endDate >= rightDate)
         {
-            var event = ConstructorTaskElement(Task, 1, 7);
+            var event = ConstructorTaskElement(Task, 1, 14);
             Task.htmlElement = event;
             User.htmlElement.lastChild.appendChild(event);
         }
@@ -434,25 +447,27 @@ function UpdateDatesLine() {
         month: "numeric"
     });
     dateArray = [];
-    for (var i = 0; i < 7; i++)
+    for (var i = 0; i < 14; i++)
     {
         var numberDay = numbersDays[i];
         var tempDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() - (weekDay - i) + 7 * navWeek);
+        tempDate = new Date(tempDate.setHours(4));
         if (i === 0)
         {
             leftDate = tempDate;
         }
-        if (i === 3)
+        if (i === 6)
         {
             nowDate.innerText = `${formatForHead.format(tempDate)}`; 
         }
-        if (i === 6)
+        if (i === 13)
         {
             rightDate = tempDate;
         }
         numberDay.innerText = formatForDays.format(tempDate);
         dateArray.push(tempDate);
     }
+    
 }
 // изменение навигации
 function ChangeNav(num) {
